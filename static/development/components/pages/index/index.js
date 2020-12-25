59688166f65fa1cf4486fecb27ff999fa90aad92
VitaliyVa <vistet1428@gmail.com>
2020-12-25 15:55:29 +0200

    repush

---
+++ static/development/components/pages/index/index.js
@@ -0,0 +1,46 @@
+import './index.scss';
+
+
+return_anim_item('.main_content_profile', 'anim_bottom', 100, 500);
+
+
+
+function return_anim_item(item, className, delay, timeOut) {
+    let custom_delay = delay;
+    let all_target = $(item);
+    $(all_target).css('position', 'relative');
+    $(all_target).css('opacity', '0');
+
+    setTimeout(() => {
+        $.each(all_target, function (index, litle_value) {
+            custom_delay += delay;
+            set_scroll(litle_value, function (value) {
+                setTimeout(() => {
+                    $(value).addClass(className);
+                    $('.main_bg').addClass('main_bg_active');
+                }, custom_delay);
+            });
+        });
+    }, timeOut);
+}
+
+function set_scroll(container, action) {
+    let target_block = $(container);
+    let blockStatus = true;
+    $(window).scroll(function () {
+        for_eacher(target_block);
+    });
+    for_eacher(target_block);
+
+    function for_eacher(block) {
+        $.each(block, function (index, value) {
+            let scrollEvent = ($(window).scrollTop() >= ($(value).position().top - $(window).height()));
+
+            if (scrollEvent && blockStatus) {
+                blockStatus = false;
+                action(value);
+            }
+        });
+    }
+}
+
